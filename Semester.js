8d5cf479bb4c1e451f358f6b2120fad55aaee0e3
Saketha Mahakala <sakethareddy@Sakethas-MBP.lan>
2022-02-14 19:05:48 -0500

    JSON Object

---
+++ Semester.js
@@ -0,0 +1,176 @@
+let semester1=[{
+    "subject":"DataMining",
+    "semester":"spring",
+    "startdate":"10thJan",
+    "enddate":"6thMay",
+    "professor":"Yousef",
+    "day":"friday",
+    "time":"8:30am",
+    "course":"elective",
+    "credit":3,
+    "gpa":4
+},{
+    "subject":"C#.Net",
+    "semester":"spring",
+    "startdate":"10thJan",
+    "enddate":"6thMay",
+    "professor":"Ahmed",
+    "day":"saturday",
+    "time":"11:30am",
+    "course":"elective",
+    "credit":3,
+    "gpa":3
+
+},{
+    "subject":"BigData",
+    "semester":"spring",
+    "startdate":"10thJan",
+    "enddate":"6thMay",
+    "professor":"Mohmmad",
+    "day":"Wednesday",
+    "time":"6:30pm",
+    "course":"elective",
+    "credit":3,
+    "gpa":3
+}];
+let semester2=[{
+    "subject":"Adv.OperatingSystems",
+    "semester":"Summer1",
+    "startdate":"17thMay",
+    "enddate":"10thJune",
+    "professor":"Park",
+    "day":"Monday",
+    "time":"6:20pm",
+    "course":"core",
+    "credit":3,
+    "gpa":4
+},{
+    "subject":"Compiler",
+    "semester":"Summer1",
+    "startdate":"17thMay",
+    "enddate":"10thJune",
+    "professor":"chenHang",
+    "day":"Tuesday",
+    "time":"10:00am",
+    "course":"core",
+    "credit":3,
+    "gpa":4
+},{
+    "subject":"DatabaseSystems",
+    "semester":"Summer2",
+    "startdate":"15thJune",
+    "enddate":"8thAugust",
+    "professor":"Zhang",
+    "day":"wednesday",
+    "time":"1:30pm",
+    "course":"elective",
+    "credit":3,
+    "gpa":4
+},{
+     "subject":"Java",
+    "semester":"Summer2",
+    "startdate":"15thJune",
+    "enddate":"8thAugust",
+    "professor":"Trang",
+    "day":"Monday",
+    "time":"10:00am",
+    "course":"prerequisite",
+    "credit":3,
+    "gpa":4
+}];
+let semester3=[{
+    "subject":"Adv.Algorithms",
+    "semester":"Fall",
+    "startdate":"10thAugust",
+    "enddate":"10thDecember",
+    "professor":"Yousef",
+    "day":"Friday",
+    "time":"8:30am",
+    "course":"core",
+    "credit":3,
+    "gpa":4
+    
+},{
+    "subject":"Adv.Database",
+    "semester":"Fall",
+    "startdate":"10thAugust",
+    "enddate":"10thDecember",
+    "professor":"Mohmmad",
+    "day":"Saturday",
+    "time":"8:00am",
+    "course":"core",
+    "credit":3,
+    "gpa":4
+},{
+    "subject":"Adv.Java",
+    "semester":"Fall",
+    "startdate":"10thAugust",
+    "enddate":"10thDecember",
+    "professor":"SunaeShin",
+    "day":"Saturday",
+    "time":"11:30am",
+    "course":"elective",
+    "credit":3,
+    "gpa":3
+}];
+let masterDegree={
+    "name":"Comp.Sci",
+    "university":{
+        "name":"Ucm",
+        "address":{
+            "street":"Lee Summit",
+            "city":"Kansas",
+            "tate":"Missouri",
+            "phoneNumber":"614-285-8777"
+
+        }
+
+    },
+    "cgpa":3.7,
+    "semesters":[{
+        "courses":[{
+            "name":"Data Mining",
+            "credits":3,
+            "instructor":"Yosef",
+            "grage":"A",
+            "type":"Elective",
+            "isOnline":false,
+            "date":"Firday",
+            "time":"8:30am",
+            "noOfDays":2,
+            "intake":"Spring",
+        },{
+            "name":"C#.Net",
+            "credits":3,
+            "instructor":"Ahmed",
+            "grage":"A",
+            "type":"Elective",
+            "isOnline":false,
+            "date":"Sat",
+            "time":"11:30am",
+            "noOfDays":2,
+            "intake":"Spring",
+
+
+        },{
+            "name":"BigData",
+            "credits":3,
+            "instructor":"Mohmmad",
+            "grage":"A",
+            "type":"Elective",
+            "isOnline":false,
+            "date":"Wednesday",
+            "time":"0",
+            "noOfDays":2,
+            "intake":"Spring",
+
+        }]
+
+    },{
+
+    },{
+
+    },{
+
+    }]
+}
---
+++ Sony.js
@@ -0,0 +1,91 @@
+let productInfo={
+    "productTitle":"Sony WH-XB910N EXTRA BASS Noise Cancelling Headphones, Wireless Bluetooth Over the Ear Headset with Microphone and Alexa Voice Control, Black",
+    "originalprice":"$249.99",
+    "discountprice":"$148.00",
+    "savedAmount":"$101.99",
+    "FreeReturns":"True",
+    "Manufacturer":"sony",
+    "itemWeight":"1pounds",
+    "Batteries":"1 lithium ion",
+    "EarPlacement":"OverEar",
+    "color":"Black",
+    "connectivitytechnology":"wireless",
+    "ModelName":"WHXB910N/B",
+    "soldby":"Amazon.com",
+    "bestsellersrank":46,
+    "isdiscontinuednymanufacturer":"False",
+    "Datefirstavailable":"sepetember 29,2021",
+    "Countryoforigin":"Vietnam"
+
+}
+
+let customerReviews={
+    "gloabalRatings":251,
+    "starRating5":69,
+    "starRating4":17,
+    "starRating3":7,
+    "starRating2":3,
+    "starRating1":4,
+    "maxRating":5,
+
+
+}
+
+let byFeatureRating={
+    "batteryLife":4.6,
+    "bassQuality":4.3,
+    "noiseCamcellation":4.2,
+    "soundQuality":4.0  
+}
+
+let reviews={
+    "types":[{
+        "type1":"topreviews"
+    },{
+        "type2":"mostrecent"
+    }],
+        "topreviews":[{
+            "reviewerName":"B.Willis",
+            "ratingGiven":3.0,
+            "review":"I own many headphones including Bose overear, Bose Earbuds, Cleer Enduro ANC, Bopman and others...",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"Nov 3, 2021",
+            "color":"black"
+        },{
+            "reviewerName":"Shawn",
+            "ratingGiven":5.0,
+            "review":"My best birthday gift ever!",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"Nov 1, 2021",
+            "color":"blue"
+        },{
+            "reviewerName":"Dave Williams",
+            "ratingGiven":2.0,
+            "review":"These headphones are just depressing.",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"Nov 9, 2021",
+            "color":"black"
+
+        }],
+        "mostrecenteeviews":[{
+            "reviewerName":"Ash",
+            "ratingGiven":5.0,
+            "review":"The extra bass is worth it",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"December 28, 2021",
+            "color":"blue"
+        },{
+            "reviewerName":"Madison",
+            "ratingGiven":3.0,
+            "review":"Also own the 900N model",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"December 27, 2021",
+            "color":"blue"
+        },{
+            "reviewerName":"Jack Hosey",
+            "ratingGiven":5.0,
+            "review":"You get what you pay for basically, good value really.",
+            "reviewedIn":"UnitedStates",
+            "reviewedOn":"December 26, 2021",
+            "color":"black"
+        }]}
